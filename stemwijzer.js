7df88179ac11686a0e720be9666d94e8ffe910f7
kiran-kovacic <99048377@mydavinci.nl>
2019-02-12 15:47:00 +0100

    meer functies

--- stemwijzer.js
+++ stemwijzer.js
@@ -5,25 +5,79 @@ const button_1    = document.createElement("BUTTON");
 const button_2    = document.createElement("BUTTON");
 const button_3    = document.createElement("BUTTON");
 const back        = document.createElement("BUTTON");
+const next        = document.createElement("BUTTON");
+// const all_parties = ["PVV", "SP", "D66", "GroenLinks", "Partij voor de Dieren",
+//                      "50Plus", "VNL", "Nieuwe Wegen", "Forum voor Democratie",
+//                      "De Burger Beweging", "Vrijzinnige Partij", "Piratenpartij",
+//                      "Libertarische Partij", "Lokaal in de Kamer", "VVD",
+//                      "PvdA", "CDA", "ChristenUnie", "SGP", "OndernemersPartij",
+//                      "DENK", "Artikel 1"]
+// var parties       = {
+//       "PVV":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "SP":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "D66":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "GroenLinks":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Partij voor de Dieren":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "50Plus":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "VNL":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Nieuwe Wegen":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Forum voor Democratie":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "De Burger Beweging":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Vrijzinnige Partij":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Piratenpartij":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Libertarische Partij":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Lokaal in de Kamer":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "VVD":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "PvdA":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "CDA":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "ChristenUnie":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "SGP":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "OndernemersPartij":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "DENK":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       "Artikel 1":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+// }
 var subject_index = 0
 var answer = []
 
 function onloadsite()
 {
   start.setAttribute("onclick", "post_statement()");
-  button_1.innerHTML  = "eens";
-  button_2.innerHTML  = "geen van bijde";
-  button_3.innerHTML  = "oneens";
-  back.innerHTML      = "<--";
+  button_1.innerHTML      = "eens";
+  button_2.innerHTML      = "geen van bijde";
+  button_3.innerHTML      = "oneens";
+  back.innerHTML          = "<--";
+  next.innerHTML          = "Sla deze vraag over -->"
+  parties.forEach(function(x, y) {
+     parties[y].totalscore = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  });
+}
+
+function check_status(text)
+{
+  for (var i = 0; i < text.length; i++) {
+    if (text[i].position == answer[subject_index]) {
+      x = text[i].name;
+      for (var z = 0; z < parties.length; z++) {
+        if (parties[z].name == x) {
+          parties[z].totalscore[subject_index] = 1;
+        }
+      }
+      console.log(x);
+    }
+  }
 }
 
 function pro_stmt()
 {
   answer[subject_index] = "pro";
+  var text = subjects[subject_index].parties;
   if (subject_index < 11) {
+    check_status(text)
+    console.log(parties);
     subject_index++;
     post_statement();
   } else {
+    check_status(text)
     finiched();
   }
 }
@@ -31,10 +85,13 @@ function pro_stmt()
 function ambivalent()
 {
   answer[subject_index] = "ambivalent";
+  var text = subjects[subject_index].parties;
   if (subject_index < 11) {
+    check_status(text)
     subject_index++;
     post_statement();
   } else {
+    check_status(text)
     finiched();
   }
 }
@@ -42,14 +99,16 @@ function ambivalent()
 function contra()
 {
   answer[subject_index] = "contra";
+  var text = subjects[subject_index].parties;
   if (subject_index < 11) {
+    check_status(text)
     subject_index++;
     post_statement();
   } else {
+    check_status(text)
     finiched();
   }
 }
-
 
 function post_statement()
 {
@@ -64,26 +123,44 @@ function post_statement()
     title.innerHTML        = number + ". " + text["title"];
     statement.innerHTML    = text["statement"];
     button_1.style.display = "inline";
-    console.dir(subject_index)
     button_1.setAttribute("onclick", "pro_stmt()");
     button_2.setAttribute("onclick", "ambivalent()");
     button_3.setAttribute("onclick", "contra()");
-    back.setAttribute("onclick", "statements(subject_index--)");
+    back.setAttribute("onclick", "post_statement(subject_index--)");
+    document.body.appendChild(next);
+    next.setAttribute("onclick", "post_statement(subject_index++)");
   } else {
     subject_index = 0;
   }
 }
 
+function getSum(total, num) {
+  return total + num;
+}
+
 function finiched()
 {
-  var text = subjects[0].parties;
-  title.innerHTML         = "hello";
-  for (var i = 0; i < text.length; i++) {
-    statement.innerHTML   = text[i].position;
+  totalscore = 4
+  for (var i = 0; i < parties.length; i++) {
+    parties[i].totalscore = parties[i].totalscore.reduce(getSum);
+    console.log(parties[i].totalscore);
+    if (parties[i].totalscore >= totalscore) {
+      totalscore = parties[i].totalscore;
+    }
   }
+  // for (var i = 0; i < all_parties.length; i++) {
+  //   var partie = all_parties[i];
+  //   if (parties[partie] == totalscore) {
+  //     statement.innerHTML = parties[name];
+  //   }
+  // }
+  title.innerHTML         = "Uitkomst";
+  statement.innerHTML     = "";
+  console.log(parties)
   start.style.display     = "none";
   button_1.style.display  = "none";
   button_2.style.display  = "none";
   button_3.style.display  = "none";
   back.style.display      = "none";
+  next.style.display      = "none";
 }
